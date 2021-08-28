1e52368f1317c0e28d9d95b45865a4706e55f598
Stella Kibika <stellakibika@gmail.com>
2021-08-28 13:02:24 +0300

    modify discretizing script

--- scripts/discretizing.py
+++ scripts/discretizing.py
@@ -1,6 +1,6 @@
 # make outcome column the first column in the dataframe
 
-def discretize_outcome(df):
+def discretize_outcome(df, causal_data):
     data_vals = {col: causal_data[col].unique() for col in causal_data.columns}
 
     diagnosis_map = {v: 'benign' if v == [0]
@@ -9,9 +9,10 @@ def discretize_outcome(df):
     return df
 
 
-def discretize_independent(df):
-    for i in list(discretised_data.columns[1:]):
-        map = {v: 'small' if v <= (discretised_data[str(i)].max() - discretised_data[str(i)].min()) / 2
+def discretize_independent(df,causal_data):
+    data_vals = {col: causal_data[col].unique() for col in causal_data.columns}
+    for i in list(discretize_outcome(df, causal_data).columns[1:]):
+        map = {v: 'small' if v <= (discretize_outcome(df, causal_data)[str(i)].max() - discretize_outcome(df, causal_data)[str(i)].min()) / 2
         else 'large' for v in data_vals[str(i)]}
-        discretised_data[str(i)] = discretised_data[str(i)].map(map)
+        discretize_outcome(df, causal_data)[str(i)] = discretize_outcome(df, causal_data)[str(i)].map(map)
     return df
