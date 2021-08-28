f60c0ee2b6487c7051efc3d704a051c4dff032dd
Stella Kibika <stellakibika@gmail.com>
2021-08-28 11:29:33 +0300

    update causal script

--- scripts/causal.py
+++ scripts/causal.py
@@ -19,11 +19,11 @@ from causalnex.evaluation import roc_auc
 from causalnex.evaluation import classification_report
 from causalnex.network import BayesianNetwork
 
-from .feature_selection import *
-from .causal_graph import *
-from .similarity import *
-from .discretizing import *
-from .training import *
+from feature_selection import *
+from causal_graph import *
+from similarity import *
+from discretizing import *
+from training import *
 
 
 import dvc
